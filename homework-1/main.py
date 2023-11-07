3d36ac238f1aefc32b21d614194f5032cd3dd29f
Konstantin Onishchenko <kostik80_80@mail.ru>
2023-11-07 19:39:02 +0300

    все сделал

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,8 +1,9 @@
 from src.channel import Channel
-
+import pprint
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
     moscowpython.print_info()
+    pprint.pprint(moscowpython.print_info())
 
     """
 {
