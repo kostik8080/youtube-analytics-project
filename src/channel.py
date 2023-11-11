3dfcf4749e3ddc15eb43845fc5aa3c831737e599
Konstantin Onishchenko <kostik80_80@mail.ru>
2023-11-11 20:37:41 +0300

    сделал 2 задание

--- src/channel.py
+++ src/channel.py
@@ -4,8 +4,6 @@ import os
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
-import isodate
-
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
 
@@ -19,8 +17,43 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.service = self.get_service()
+        self.channel_data = self.get_channel_data()
+        self.title = self.channel_data['snippet']['title']
+        self.description = self.channel_data['snippet']['description']
+        self.url = "https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A"
+        self.subscriber_count = self.channel_data['statistics']['subscriberCount']
+        self.video_count = self.channel_data['statistics']['videoCount']
+        self.view_count = self.channel_data['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         return channel
+
+    @classmethod
+    def get_service(cls):
+        api_service_name = "youtube"
+        api_version = "v3"
+        DEVELOPER_KEY = api_key
+        return build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+    def get_channel_data(self):
+        request = self.service.channels().list(
+            part="snippet,statistics",
+            id=self.channel_id
+        )
+        response = request.execute()
+        return response['items'][0]
+
+    def to_json(self, filename):
+        with open(filename, 'w') as f:
+            json.dump({
+                'channel_id': self.channel_id,
+                'title': self.title,
+                'description': self.description,
+                'custom_url': self.url,
+                'subscriber_count': self.subscriber_count,
+                'video_count': self.video_count,
+                'view_count': self.view_count
+            }, f)
